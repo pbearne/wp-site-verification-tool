1d5f393d1f46eb16c646f13affbf6f1033e49424
Paul Bearne <pbearne@gmail.com>
2013-09-13 18:38:38 -0400

    sort stript[D[D[D[D[D[D[Ding deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@
 
 # main config
 PLUGINSLUG="wp-site-verification-tool"
-CURRENTDIR=`pwd`
+CURRENTDIR='./'
 MAINFILE="wp-site-verification-tool.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR" # this file should be in the base of your git repository
 
 # svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+SVNPATH="../tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/wp-site-verification-tool/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="pbearne" # your svn username
 
