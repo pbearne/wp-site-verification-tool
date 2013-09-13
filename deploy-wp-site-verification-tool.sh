d8c09b66ca0f2d8c945ce04e1e3e6ea14e6de72b
Paul Bearne <pbearne@gmail.com>
2013-09-12 20:02:50 -0400

    first push

--- deploy-wp-site-verification-tool.sh
+++ deploy-wp-site-verification-tool.sh
@@ -3,16 +3,16 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
-PLUGINSLUG="author-avatars"
+PLUGINSLUG="wp-site-verification-tool"
 CURRENTDIR=`pwd`
-MAINFILE="author-avatars.php" # this should be the name of your main php file in the wordpress plugin
+MAINFILE="wp-site-verification-tool.php" # this should be the name of your main php file in the wordpress plugin
 
 # git config
 GITPATH="$CURRENTDIR" # this file should be in the base of your git repository
 
 # svn config
-SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/author-avatars/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNPATH="/temp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
+SVNURL="http://plugins.svn.wordpress.org/wp-site-verification-tool/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="pbearne" # your svn username
 
 
